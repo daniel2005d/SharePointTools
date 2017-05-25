896a0ee0ee52f35d3b8a9ccb89ed3650cf950431
daniel2005d <daniel.vargas@outlook.com>
2017-05-25 18:08:49 -0500

    Add files via upload

--- readxml.py
+++ readxml.py
@@ -2,8 +2,13 @@ import sys
 import xml.etree.ElementTree as ET
 import urllib2
 
+pageadmin = ['/_layouts/settings.aspx','/_layouts/viewlsts.aspx']
+
 def leersitemap():
-   response = urllib2.urlopen('http://www.avianca.com/es-co/sitemap.xml')
+   proxy = urllib2.ProxyHandler({'http': 'porvenirafp\por02408:Porvenir*69655@172.17.10.3:8080'})
+   opener = urllib2.build_opener(proxy)
+   urllib2.install_opener(opener)
+   response = urllib2.urlopen('https://www.colmedica.com/sitemap.xml')
    f = open('sitemap.xml','w')
    if (response.getcode() == 200):
        sitemap = response.read()
@@ -26,13 +31,26 @@ def crearurls(xml):
     return urls
 
 def payload(direcciones):
+    global pageadmin
+    paginas = []
     for d in direcciones:
-	   parts = d.split('/')
-	   segments = ' '.join(parts).split()
-	   print "====================="
-	   for p in segments:
-	      
+       parts = d.split('/')
+       segments = ' '.join(parts).split()
+       url = ''
+       for p in range(len(segments)):
+           
+           if p<len(segments)-1:
+             if p == 0:
+                url = url+ segments[p]+'/'
+             else:
+                url = url +'/' + segments[p]
+       for a in pageadmin:
+            paginas.append(url+a)
+    return paginas
    
 xml = leersitemap()
 direcciones = crearurls(xml)
-payload(direcciones)
+paginas = payload(direcciones)
+for p in paginas:
+   response = urllib2.urlopen(p)
+   print response.getcode(),p
